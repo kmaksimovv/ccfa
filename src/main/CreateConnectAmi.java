33321f6c66ff72cd79fc525320a71019d327f013
Максимов Константин Владимирович <18905320@sigma.sbrf.ru>
2021-03-02 18:36:28 +0400

    added method to class CreateConnectAmi

--- src/main/CreateConnectAmi.java
+++ src/main/CreateConnectAmi.java
@@ -5,16 +5,34 @@ import java.util.Properties;
 
 public class CreateConnectAmi {
 
-    public void connectionAmi() throws IOException {
+    public Properties connectionAmi() throws IOException {
+
         FileInputStream fileInputStream = null;
         Properties properties = new Properties();
-        String fileName = "config.properties";
-
         try {
+
+            String fileName = "config.properties";
             fileInputStream = new FileInputStream(fileName);
             properties.load(fileInputStream);
-        } catch (FileNotFoundException e){
+            parseProperties(properties);
 
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка! Файл config.properties не найден!");
+        } finally {
+            if (fileInputStream != null) {
+                fileInputStream.close();
+            }
         }
+
+        return properties;
+    }
+
+    public void parseProperties(Properties properties) {
+        String login = properties.getProperty("ami.login");
+        String password = properties.getProperty("ami.password");
+        String host = properties.getProperty("ami.host");
+        String port = properties.getProperty("ami.port");
+
+        System.out.println("login: " + login + "\r\n" + "password: " + password + "\r\n" + "host: " + host + "\r\n" + "port: " + port + "\r\n");
     }
 }
