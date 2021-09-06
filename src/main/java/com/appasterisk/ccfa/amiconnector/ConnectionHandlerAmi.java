3399732e0c33ec8ada001b686bbc4cf93b46f12f
mako <kmaksimovv@gmail.com>
2021-09-06 22:19:30 +0400

    parse data events from ami

--- src/main/java/com/appasterisk/ccfa/amiconnector/ConnectionHandlerAmi.java
+++ src/main/java/com/appasterisk/ccfa/amiconnector/ConnectionHandlerAmi.java
@@ -1,42 +1,43 @@
 package com.appasterisk.ccfa.amiconnector;
 
 import java.io.*;
-import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionHandlerAmi extends Thread {
 
-    private static Socket clientSocket;
-    private static BufferedReader inReadLine;
-    private static BufferedWriter out;
+    private Socket clientSocket;
+    private BufferedReader inReadLine;
+    private BufferedWriter out;
+    private HashMap<String, String> mapEvents = new HashMap<>();
 
     public void run() {
         try {
             createConnection();
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void createConnection() throws IOException, InterruptedException {
+    public void createConnection() throws IOException {
         try {
-            String readData;
             PropsAmi propsAmi = new PropsAmi();
             propsAmi.loadConfigForConnect();
             clientSocket = new Socket();
             clientSocket.connect(new InetSocketAddress(propsAmi.getHost(), propsAmi.getPort()), 2000);
-
             inReadLine = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
             out.write(Action.loginAction(propsAmi.getLogin(), propsAmi.getPassword()));
             out.flush();
 
+            String data;
             while (true) {
                 if (inReadLine.ready()) {
-                    readData = inReadLine.readLine();
-                    System.out.println(readData);
+                    data = inReadLine.readLine();
+                    parseDataEventsAmi(data);
                 }
             }
         } catch (SocketTimeoutException e) {
@@ -46,6 +47,23 @@ public class ConnectionHandlerAmi extends Thread {
             System.out.println("Не удалось подключиться к серверу");
             createConnection();
         }
+    }
+
+    private void parseDataEventsAmi(String data) {
+
+        if (data.contains(":")) {
+            String[] dataSplit = data.split(":");
+            mapEvents.put(dataSplit[0], dataSplit[1]);
+
+            for (Map.Entry<String, String> entry : mapEvents.entrySet()) {
+                System.out.println("key: " + entry.getKey() + ", " + "value: " + entry.getValue());
+            }
+
+        }
+    }
+
+    private void checkLogin() {
+
     }
 }
 
